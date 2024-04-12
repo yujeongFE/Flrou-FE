bd08bfb17bd4e709e1691e37d72a597c47c29017
yujeongFE <yujeong6231@naver.com>
2024-04-12 14:43:42 +0900

    chore: 새로운 폰트 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Splash from "./pages/Splash/index";
 import Chatting from "./pages/Chatting/index";
