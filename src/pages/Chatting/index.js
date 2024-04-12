729e57b6f7628c4cf20224ce828635886b02db2d
yujeongFE <yujeong6231@naver.com>
2024-04-12 16:39:10 +0900

    feat: 채팅 페이지 버튼 추가

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,10 +1,34 @@
-import React from "react";
-import { Container } from "./style";
+import React, { useState } from "react";
+import PrimaryButton from "../../components/Button/PrimaryButton";
+import { Container, ButtonContainer } from "./style";
 
 const Index = () => {
-  return <Container>
-    <span>FLROU 채팅 페이지</span>
-  </Container>;
+  const [isCalender, setIsCalender] = useState(false);
+  const [isTodo, setIsTodo] = useState(false);
+
+  const handleCalender = () => {
+    {
+      !isTodo && setIsCalender((prevIsActive) => !prevIsActive);
+    }
+  };
+  const handleToDo = () => {
+    {
+      !isCalender && setIsTodo((prevIsActive) => !prevIsActive);
+    }
+  };
+
+  return (
+    <Container>
+      <ButtonContainer>
+        <PrimaryButton onClick={handleCalender} isActive={isCalender}>
+          캘린더 입력
+        </PrimaryButton>
+        <PrimaryButton onClick={handleToDo} isActive={isTodo}>
+          TODO 입력
+        </PrimaryButton>
+      </ButtonContainer>
+    </Container>
+  );
 };
 
 export default Index;
--- src/pages/Chatting/style.js
+++ src/pages/Chatting/style.js
@@ -3,8 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+`;
+
+export const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  margin-top: 20px;
+  gap: 8vw;
 `;
