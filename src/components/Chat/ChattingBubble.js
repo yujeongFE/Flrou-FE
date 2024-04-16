8190288e5ccacc257608976b6f2cd263d8498c8b
yujeongFE <yujeong6231@naver.com>
2024-04-16 12:40:12 +0900

    style: 말풍선 fade 애니메이션 적용

--- src/components/Chat/ChattingBubble.js
+++ src/components/Chat/ChattingBubble.js
@@ -1,8 +1,19 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import PropTypes from "prop-types";
 
 import Character from "../../assets/flrou_character.png";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0.1;
+    transform: translateY(1.3vh);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 const MessageContainer = styled.div`
   display: flex;
@@ -17,7 +28,6 @@ const MyMessageBubble = styled.div`
   padding: 10px;
   margin-bottom: 2.7vh;
   text-align: center;
-
   color: #2c2c2c;
   font-family: "Pretendard";
   font-size: 22px;
@@ -26,13 +36,13 @@ const MyMessageBubble = styled.div`
   line-height: 17px;
   letter-spacing: 0.22px;
   position: relative;
+  animation: ${fadeIn} 0.5s ease-in-out;
 `;
 
 const OpponentMessageContainer = styled.div`
   display: flex;
   align-items: center;
   flex-direction: row;
-  gap: 10px;
 `;
 
 const OpponentMessageBubble = styled(MyMessageBubble)`
