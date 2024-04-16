e4381d55d5acd7cb8fe76e42aedff266170a4544
yujeongFE <yujeong6231@naver.com>
2024-04-16 13:32:35 +0900

    feat: 채팅 대화 말풍선에 렌더링

--- src/components/Chat/ChattingBubble.js
+++ src/components/Chat/ChattingBubble.js
@@ -27,13 +27,14 @@ const MyMessageBubble = styled.div`
   border-radius: 12px;
   padding: 10px;
   margin-bottom: 2.7vh;
+
   text-align: center;
   color: #2c2c2c;
   font-family: "Pretendard";
   font-size: 22px;
   font-style: normal;
   font-weight: 500;
-  line-height: 17px;
+  line-height: 1.5
   letter-spacing: 0.22px;
   position: relative;
   animation: ${fadeIn} 0.5s ease-in-out;
@@ -41,11 +42,13 @@ const MyMessageBubble = styled.div`
 
 const OpponentMessageContainer = styled.div`
   display: flex;
+  height: auto;
   align-items: center;
   flex-direction: row;
 `;
 
 const OpponentMessageBubble = styled(MyMessageBubble)`
+  max-width: 70%;
   display: flex;
   align-items: center;
   margin-top: 2.7vh;
@@ -58,27 +61,80 @@ const CharacterImage = styled.img`
   margin-right: 10px;
 `;
 
-const ChattingBubble = ({ messages }) => {
+const ChattingBubble = ({ messages, scheduleMessage, todoMessage, isCalender, isTodo }) => {
   return (
     <MessageContainer>
-      <OpponentMessageContainer>
-        <CharacterImage src={Character} alt="character" />
-        <OpponentMessageBubble isMine={false}>{"오늘 일정을 말씀해주세요~!"}</OpponentMessageBubble>
-      </OpponentMessageContainer>
-      {messages.map((message, index) => (
-        <React.Fragment key={index}>
-          {message.isMine ? (
-            <MyMessageBubble isMine={message.isMine} textLength={message.text.length}>
+      {/* 일정 메시지 */}
+      {isCalender && (
+        <>
+          <OpponentMessageContainer>
+            <CharacterImage src={Character} alt="character" />
+            <OpponentMessageBubble isMine={false}>{"일정을 말씀해주세요~!"}</OpponentMessageBubble>
+          </OpponentMessageContainer>
+          {scheduleMessage.map((message, index) =>
+            message.isMine ? (
+              <MyMessageBubble key={index} isMine={true} textLength={message.text.length}>
+                {message.text}
+              </MyMessageBubble>
+            ) : (
+              <OpponentMessageContainer key={index}>
+                <CharacterImage src={Character} alt="character" />
+                <OpponentMessageBubble isMine={false}>{message.text}</OpponentMessageBubble>
+              </OpponentMessageContainer>
+            ),
+          )}
+          {/* 일정 등록이 완료된 경우 
+          <OpponentMessageContainer key={index}>
+            <CharacterImage src={Character} alt="character" />
+            <OpponentMessageBubble isMine={false}>{"일정 등록이 완료되었어요~ 캘린더에서 확인해보세요!"}</OpponentMessageBubble>
+          </OpponentMessageContainer>
+            */}
+        </>
+      )}
+
+      {/* 할 일 메시지 */}
+      {isTodo && (
+        <>
+          <OpponentMessageContainer>
+            <CharacterImage src={Character} alt="character" />
+            <OpponentMessageBubble isMine={false}>{"오늘의 할일을 말씀해주세요~!"}</OpponentMessageBubble>
+          </OpponentMessageContainer>
+          {todoMessage.map((message, index) =>
+            message.isMine ? (
+              <MyMessageBubble key={index} isMine={true} textLength={message.text.length}>
+                {message.text}
+              </MyMessageBubble>
+            ) : (
+              <OpponentMessageContainer key={index}>
+                <CharacterImage src={Character} alt="character" />
+                <OpponentMessageBubble isMine={false}>{message.text}</OpponentMessageBubble>
+              </OpponentMessageContainer>
+            ),
+          )}
+          {/* 일정 등록이 완료된 경우 
+          <OpponentMessageContainer key={index}>
+            <CharacterImage src={Character} alt="character" />
+            <OpponentMessageBubble isMine={false}>{"일정 등록이 완료되었어요~ todo에서 확인해보세요!"}</OpponentMessageBubble>
+          </OpponentMessageContainer>
+            */}
+        </>
+      )}
+
+      {/* 일반 대화 메시지 */}
+      {!isCalender &&
+        !isTodo &&
+        messages.map((message, index) =>
+          message.isMine ? (
+            <MyMessageBubble key={index} isMine={true} textLength={message.text.length}>
               {message.text}
             </MyMessageBubble>
           ) : (
-            <OpponentMessageBubble isMine={message.isMine}>
+            <OpponentMessageContainer key={index}>
               <CharacterImage src={Character} alt="character" />
-              {message.text}
-            </OpponentMessageBubble>
-          )}
-        </React.Fragment>
-      ))}
+              <OpponentMessageBubble isMine={false}>{message.text}</OpponentMessageBubble>
+            </OpponentMessageContainer>
+          ),
+        )}
     </MessageContainer>
   );
 };
@@ -90,6 +146,20 @@ ChattingBubble.propTypes = {
       isMine: PropTypes.bool.isRequired,
     }),
   ).isRequired,
+  isCalender: PropTypes.bool.isRequired,
+  scheduleMessage: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      isMine: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+  todoMessage: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      isMine: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+  isTodo: PropTypes.bool.isRequired,
 };
 
 export default ChattingBubble;
