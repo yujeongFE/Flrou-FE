574d3205639eccd0356c18e1ce02079496386b36
yujeongFE <yujeong6231@naver.com>
2024-05-15 02:01:17 +0900

    refactor: 로그인 여부에 따른 페이지 이동 경로 변경

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Container, Logo, WelcomeButton } from "./style";
-import FLROU from "../../assets/flrou_logo.png";
+import FLROULogo from "../../assets/flrou_logo.png";
 import { useNavigate } from "react-router-dom";
 
 const Index = () => {
   const navigate = useNavigate();
   const handleButtonClick = () => {
-    // 로그인한 경우에만 조건부 라우팅 작업 예정
-    navigate("/chatting");
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      navigate("/login");
+    } else {
+      navigate("/chatting");
+    }
   };
 
   return (
     <Container>
-      <Logo src={FLROU} />
-      <WelcomeButton onClick={handleButtonClick}>나만의 일정 비서 FLROU 시작하기 {" >"} </WelcomeButton>
+      <Logo src={FLROULogo} alt="FLROU 로고" />
+      <WelcomeButton onClick={handleButtonClick}>나만의 일정 비서 FLROU 시작하기 {" >"}</WelcomeButton>
     </Container>
   );
 };
