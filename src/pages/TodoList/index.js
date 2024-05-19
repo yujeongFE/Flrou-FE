4eaa56c8e221eefd9256f70676b6d94c8f8a1b6f
yujeongFE <yujeong6231@naver.com>
2024-05-19 16:21:14 +0900

    feat: 투두리스트 페이지 초기 세팅

---
+++ src/pages/TodoList/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../layout/Header";
+import { Container } from "./style";
+
+const Index = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Index;
