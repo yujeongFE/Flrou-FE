c977694191cc75e60663879f664d21397b4072bb
9oodam <godamcity98@gmail.com>
2024-05-29 18:42:09 +0900

    feat: todo edit: 완료/미완료 탭 합치기

--- src/pages/TodoList/index.js
+++ src/pages/TodoList/index.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Header from "../../layout/Header";
-import { Container, TodoArraow, TodoButton, TodoButton2, TodoContainer, TodoContent, TodoList, TodoSubject } from "./style";
+import { Container, TodoButton, TodoButton2, TodoContainer, TodoContent, TodoList, TodoSubject } from "./style";
+// import { Container, TodoArraow, TodoButton, TodoButton2, TodoContainer, TodoContent, TodoList, TodoSubject } from "./style";
 
 import btn1 from "../../assets/todo_btn_1.png";
 import btn2 from "../../assets/todo_btn_2.png";
@@ -13,7 +14,7 @@ const Index = () => {
   // 투두 리스트
   const [todoList, setTodoList] = useState(null);
   // 미완료, 완료 탭 구분
-  const [finished, setFinished] = useState(false);
+  // const [finished, setFinished] = useState(false);
   // 리스트 클릭 여부
   const [clicked, setClicked] = useState(false);
   // 투두 수정본
@@ -83,42 +84,20 @@ const Index = () => {
   }, [newTodo])
 
 
+  // 최종 발표 ver
   return (
     <Container>
       <Header />
       <TodoSubject>
-        {finished ? (
-          <>
-          <TodoArraow onClick={() => {setFinished(true)}} col={'lightgray'}
-          >{'<'}</TodoArraow>
           <div>Todo List</div>
-          <TodoArraow onClick={() => {
-            setFinished(false);
-            setClicked(false);
-          }}>{'>'}</TodoArraow>
-          </>
-        ) : (
-          <>
-          <TodoArraow onClick={() => {
-            setFinished(true);
-            setClicked(false);
-          }}>{'<'}</TodoArraow>
-          <div>Todo List</div>
-          <TodoArraow onClick={() => {setFinished(false)}} col={'lightgray'}>{'>'}</TodoArraow>
-          </>
-        )}
       </TodoSubject>
 
       {todoList &&
       <TodoContainer>
-        {todoList.filter((list0) => finished ? list0.isDone : !list0.isDone).map((list) => {
-          let color = '#77ADFD'
-          if(finished) {
-            color = 'lightgray';
-          }else if(!finished) {
-            color = '#77ADFD';
-          }
-          
+        {/* 미완료 탭 */}
+        {todoList.filter(list => !list.isDone).map((list) => {
+          let color = '#77ADFD';
+
           return(
             <>
             <TodoList key={list.id} col={color} onClick={() => {clickContent(list)}}>
@@ -140,16 +119,10 @@ const Index = () => {
                 <>
                 <TodoContent>{list.todo}</TodoContent>
                 <TodoButton>
-                  {finished ? (
-                    <img src={btn2} onClick={(e) => {
-                      e.stopPropagation();
-                      handleTodoBtn(list);
-                    }}></img>
-                  ) : (
                     <img src={btn1} onClick={(e) => {
                       e.stopPropagation();
                       handleTodoBtn(list);
-                    }}></img>                  )}
+                    }}></img>
                 </TodoButton>
                 </>
               )}
@@ -157,10 +130,128 @@ const Index = () => {
           </>
           )
         })}
+
+        {/* 완료 탭 */}
+        {todoList.filter(list => list.isDone).map((list) => {
+          let color = 'lightgray';
+
+          return(
+            <>
+            <TodoList key={list.id} col={color} onClick={() => {clickContent(list)}}>
+              {clicked ? (
+                <>
+                <TodoContent>
+                  <input
+                    onClick={(e) => e.stopPropagation()}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    placeholder={list.todo}
+                  />
+                </TodoContent>
+                <TodoButton onClick={(e) => {e.stopPropagation()}}>
+                  <TodoButton2 col={'#77ADFD'} onClick={() => {clickV(list)}}>V</TodoButton2>
+                  <TodoButton2 col={'red'} onClick={() => {clickX(list)}}>X</TodoButton2>
+                </TodoButton>
+                </>
+              ) : (
+                <>
+                <TodoContent>{list.todo}</TodoContent>
+                <TodoButton>
+                    <img src={btn2} onClick={(e) => {
+                      e.stopPropagation();
+                      handleTodoBtn(list);
+                    }}></img>
+                </TodoButton>
+                </>
+              )}
+            </TodoList>
+          </>
+          )
+        })}
+
       </TodoContainer>
       }
     </Container>
   );
+
+  // 최종 점검 ver.
+  // return (
+  //   <Container>
+  //     <Header />
+  //     <TodoSubject>
+  //       {finished ? (
+  //         <>
+  //         <TodoArraow onClick={() => {setFinished(true)}} col={'lightgray'}
+  //         >{'<'}</TodoArraow>
+  //         <div>Todo List</div>
+  //         <TodoArraow onClick={() => {
+  //           setFinished(false);
+  //           setClicked(false);
+  //         }}>{'>'}</TodoArraow>
+  //         </>
+  //       ) : (
+  //         <>
+  //         <TodoArraow onClick={() => {
+  //           setFinished(true);
+  //           setClicked(false);
+  //         }}>{'<'}</TodoArraow>
+  //         <div>Todo List</div>
+  //         <TodoArraow onClick={() => {setFinished(false)}} col={'lightgray'}>{'>'}</TodoArraow>
+  //         </>
+  //       )}
+  //     </TodoSubject>
+
+  //     {todoList &&
+  //     <TodoContainer>
+  //       {todoList.filter((list0) => finished ? list0.isDone : !list0.isDone).map((list) => {
+  //         let color = '#77ADFD'
+  //         if(finished) {
+  //           color = 'lightgray';
+  //         }else if(!finished) {
+  //           color = '#77ADFD';
+  //         }
+          
+  //         return(
+  //           <>
+  //           <TodoList key={list.id} col={color} onClick={() => {clickContent(list)}}>
+  //             {clicked ? (
+  //               <>
+  //               <TodoContent>
+  //                 <input
+  //                   onClick={(e) => e.stopPropagation()}
+  //                   onChange={(e) => setNewTodo(e.target.value)}
+  //                   placeholder={list.todo}
+  //                 />
+  //               </TodoContent>
+  //               <TodoButton onClick={(e) => {e.stopPropagation()}}>
+  //                 <TodoButton2 col={'#77ADFD'} onClick={() => {clickV(list)}}>V</TodoButton2>
+  //                 <TodoButton2 col={'red'} onClick={() => {clickX(list)}}>X</TodoButton2>
+  //               </TodoButton>
+  //               </>
+  //             ) : (
+  //               <>
+  //               <TodoContent>{list.todo}</TodoContent>
+  //               <TodoButton>
+  //                 {finished ? (
+  //                   <img src={btn2} onClick={(e) => {
+  //                     e.stopPropagation();
+  //                     handleTodoBtn(list);
+  //                   }}></img>
+  //                 ) : (
+  //                   <img src={btn1} onClick={(e) => {
+  //                     e.stopPropagation();
+  //                     handleTodoBtn(list);
+  //                   }}></img>                  )}
+  //               </TodoButton>
+  //               </>
+  //             )}
+  //           </TodoList>
+  //         </>
+  //         )
+  //       })}
+  //     </TodoContainer>
+  //     }
+  //   </Container>
+  // );
 };
 
 export default Index;
