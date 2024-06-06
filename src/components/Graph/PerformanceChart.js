5648c2ce8196c88d0bd2a4371446c83b2b19129e
yujeongFE <yujeong6231@naver.com>
2024-06-06 16:08:24 +0900

    feat: 완수율 계산 후 그래프 구현

--- src/components/Graph/PerformanceChart.js
+++ src/components/Graph/PerformanceChart.js
@@ -1,43 +1,48 @@
 import React, { useEffect, useState } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Area } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine } from "recharts";
 
-const PerformanceChart = (props) => {
+const PerformanceChart = ({ isActive, successCount, currentYear }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    setData(generateData(props.currentDate, props.currentYear, props.isActive));
-  }, [props.currentDate, props.currentYear, props.isActive]);
+    const generateData = () => {
+      if (successCount && successCount.length > 0) {
+        let newData = [];
 
-  const generateData = (currentDate, currentYear, isActive) => {
-    let newData = [];
-
-    if (isActive === "month") {
-      const startMonth = 1;
-      const endMonth = currentDate.getMonth() + 1;
-      for (let month = startMonth; month <= endMonth; month++) {
-        newData.push({ month: `${month}월`, performance: Math.floor(Math.random() * 100) });
+        if (isActive === "month") {
+          successCount.forEach((count, index) => {
+            const total = count[0] + count[1];
+            const completionRate = total === 0 ? 0 : Math.round((count[0] / total) * 100);
+            newData.push({ month: `${index + 1}월`, 완수율: completionRate });
+          });
+        } else {
+          let total = 0;
+          let completionRate = 0;
+          for (let i = successCount.length - 1; i >= 0; i--) {
+            total += successCount[i][0] + successCount[i][1];
+            completionRate += total === 0 ? 0 : Math.round((successCount[i][0] / total) * 100);
+          }
+          newData.push({ month: `${currentYear}년`, 완수율: completionRate });
+        }
+        setData(newData);
       }
-    } else {
-      for (let year = currentYear - 1; year <= currentYear; year++) {
-        newData.push({ year: `${year}년`, performance: Math.floor(Math.random() * 100) });
-      }
-    }
+    };
 
-    return newData;
-  };
+    generateData();
+  }, [isActive, successCount]);
 
   return (
-    <div style={{ backgroundColor: "#EDF3FB" }}>
+    <div style={{ backgroundColor: "#fff" }}>
       <ResponsiveContainer width="100%" height={400}>
-        <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <BarChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid vertical={false} />
-          {props.isActive === "month" ? <XAxis dataKey="month" /> : <XAxis dataKey="year" />}
+          <XAxis dataKey="month" />
           <YAxis />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="performance" stroke="#8884d8" fill="#ECEDFD" />
-          <Line type="monotone" dataKey="performance" stroke="#8884d8" strokeWidth={3} activeDot={{ r: 8 }} />
-        </LineChart>
+          <Tooltip formatter={(value) => `${value}%`} />
+          <Bar dataKey="완수율" fill="#63A1FD" barSize={30} />
+          <ReferenceLine y={50} stroke="#FFC2C2" strokeWidth={2} label={{ value: "50%", position: "right", dy: -5 }} />
+          {isActive !== "month" && <ReferenceLine x={data.length - 1} stroke="gray" strokeWidth={2} />}
+        </BarChart>
       </ResponsiveContainer>
     </div>
   );
--- src/pages/Graph/index.js
+++ src/pages/Graph/index.js
@@ -1,90 +1,114 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../layout/Header";
-import { Container, CenteredContent, DateTimeButton, PeriodText, PriodChangeButton } from "./style";
+import { Container, CenteredContent, DateTimeButton, PeriodText, PriodChangeButton, Center } from "./style";
 import PerformanceChart from "../../components/Graph/PerformanceChart";
 import TripleSelectButton from "../../components/Button/TripleSelectButton";
+import { CompletionRateRequest } from "../../components/api/Graph/CompletionRateRequest";
 
 const Graph = () => {
+  const user_id = localStorage.getItem("user_id");
   const [isActive, setIsActive] = useState("month");
-  const [currentDate, setCurrentDate] = useState(new Date()); // 현재 날짜 상태 추가
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [selectMonth, setSelectMonth] = useState(currentDate.getMonth() + 1);
+  const [selectYear, setSelectYear] = useState(currentDate.getFullYear());
+  const [successCount, setSuccessCount] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const reply = await CompletionRateRequest(user_id, selectYear, selectMonth);
+        setSuccessCount(reply);
+      } catch (error) {
+        console.error("데이터 가져오기 에러:", error);
+      }
+    };
+
+    fetchData();
+  }, [selectYear, selectMonth]);
+
+  // 이전 연도로 이동하는 함수
+  const handlePrevYear = () => {
+    const prevYear = selectYear - 1;
+    if (prevYear >= new Date().getFullYear()) {
+      return;
+    }
+    setSelectYear(prevYear);
+  };
+
+  // 다음 연도로 이동하는 함수
+  const handleNextYear = () => {
+    const nextYear = selectYear + 1;
+    if (nextYear > new Date().getFullYear()) {
+      return;
+    }
+    setSelectYear(nextYear);
+  };
 
   const handleButtonClick = (option) => {
     setIsActive(option);
   };
 
   const handlePrev = () => {
-    // 만약 월 선택 상태라면
     if (isActive === "month") {
-      const prevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
-      const prevYear = prevMonth.getFullYear();
-      const prevMonthIndex = prevMonth.getMonth();
-
-      // 이전 달이 현재 월보다 이전인지 확인하여 이동 제한
-      if (prevYear <= currentDate.getFullYear() || (prevYear === currentDate.getFullYear() && prevMonthIndex < currentDate.getMonth())) {
-        setCurrentDate(prevMonth);
-      }
-    }
-    // 만약 연도 선택 상태라면
-    else if (isActive === "year") {
-      const prevYear = currentDate.getFullYear() - 1;
-      // 이동하려는 연도가 현재 시점의 연도보다 큰 경우 이동을 제한
-      if (prevYear >= new Date().getFullYear()) {
+      const prevMonth = selectMonth - 1;
+      if (prevMonth < 0) {
         return;
       }
-      setCurrentDate(new Date(prevYear, currentDate.getMonth(), 1));
+      setSelectMonth(prevMonth);
+    } else if (isActive === "year") {
+      handlePrevYear();
     }
   };
 
   const handleNext = () => {
-    const nextMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
-    const nextYear = nextMonth.getFullYear();
-    const nextMonthIndex = nextMonth.getMonth();
-
-    // 다음 달이 현재 연도와 현재 월보다 이후인지 확인하여 이동 제한
-    if (nextYear > new Date().getFullYear() || (nextYear === new Date().getFullYear() && nextMonthIndex > new Date().getMonth())) {
-      setCurrentDate(new Date());
-      return;
+    if (isActive === "month") {
+      const nextMonth = selectMonth + 1;
+      if (nextMonth > 11) {
+        return;
+      }
+      setSelectMonth(nextMonth);
+    } else {
+      handleNextYear();
     }
-
-    setCurrentDate(nextMonth);
   };
 
   const getCurrentMonth = () => {
     if (isActive === "month") {
-      return `${currentDate.toLocaleString("default", { year: "numeric" })} ${currentDate.toLocaleString("default", { month: "long" })}`;
+      return `${selectYear}년 ${selectMonth}월`; // selectMonth는 0부터 시작하므로 1을 더해줌
     } else {
-      return `${currentDate.toLocaleString("default", { year: "numeric" })}`;
+      return `${selectYear}년`;
     }
   };
 
   return (
     <Container>
       <Header />
-      <DateTimeButton>
-        <TripleSelectButton options={["year", "month"]} onClick={handleButtonClick} activeOption={isActive} />
-      </DateTimeButton>
-      <PeriodText>
-        {(isActive === "month" || isActive === "year") && (
-          <>
-            <PriodChangeButton style={{ justifyContent: "flex-start" }} onClick={handlePrev}>
-              {"< "}
-            </PriodChangeButton>
-            <span>{getCurrentMonth()}</span>
-            <PriodChangeButton style={{ justifyContent: "flex-start" }} onClick={handleNext}>
-              {" >"}
-            </PriodChangeButton>
-          </>
-        )}
-      </PeriodText>
-      <CenteredContent>
-        <span>일정 완료율 그래프</span>
-      </CenteredContent>
+      <Center>
+        <PeriodText>
+          {(isActive === "month" || isActive === "year") && (
+            <>
+              <PriodChangeButton style={{ justifyContent: "flex-start" }} onClick={handlePrev}>
+                {"< "}
+              </PriodChangeButton>
+              <span>{getCurrentMonth()}</span>
+              <PriodChangeButton style={{ justifyContent: "flex-start" }} onClick={handleNext}>
+                {" >"}
+              </PriodChangeButton>
+            </>
+          )}
+        </PeriodText>
+        <CenteredContent>
+          <span>일정 완료율 그래프</span>
+        </CenteredContent>
+        <DateTimeButton>
+          <TripleSelectButton options={["year", "month"]} onClick={handleButtonClick} activeOption={isActive} />
+        </DateTimeButton>
+      </Center>
       <PerformanceChart
         style={{ marginTop: "200px" }}
-        displayMonths={isActive === "month" ? 12 : 1}
         isActive={isActive}
-        currentDate={currentDate}
-        currentYear={currentDate.getFullYear()}
+        currentYear={selectYear}
+        successCount={successCount}
       />
     </Container>
   );
