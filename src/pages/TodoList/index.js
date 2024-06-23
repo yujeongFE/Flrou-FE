135cdfda4eb8b26da8882f732f15a9966e7615f4
yujeongFE <109844803+yujeongFE@users.noreply.github.com>
2024-06-23 18:37:14 +0900

    style: todo 화면 모바일 스타일 적용

--- src/pages/TodoList/index.js
+++ src/pages/TodoList/index.js
@@ -12,7 +12,8 @@ import btn2 from "../../assets/todo_btn_2.png";
 import useIsMobile from "../../hooks/useIsMobile";
 
 const Index = () => {
-  const user_id = localStorage.getItem('user_id');
+  const user_id = localStorage.getItem("user_id");
+  const isMobile = useIsMobile();
 
   // 투두 리스트
   const [todoList, setTodoList] = useState(null);
@@ -23,27 +24,25 @@ const Index = () => {
   // 투두 수정본
   const [newTodo, setNewTodo] = useState(null);
 
-
   // 전체 투두리스트 불러오기
   const getTodo = async () => {
     const res = await axios.get(`http://localhost:3000/todo/getAllTodo/${user_id}`);
     console.log(res.data);
     setTodoList(res.data);
-  }
+  };
 
   // 버튼 클릭 시 (isDone true<->false)
   const handleTodoBtn = async (list) => {
     try {
       const res = await axios.post("http://localhost:3000/todo/updateTodoDone", {
-        todo_id: list.id
-      })
+        todo_id: list.id,
+      });
       console.log(res);
       getTodo();
     } catch (error) {
       console.log(error);
     }
-
-  }
+  };
 
   // 리스트 클릭시 (수정/삭제 요청)
   const clickContent = async (list) => {
@@ -51,41 +50,40 @@ const Index = () => {
     console.log(list.todo);
     setClicked(!clicked);
     console.log(clicked);
-  }
+  };
 
   // 수정 버튼 클릭시
   const clickV = async (list) => {
-    if(!newTodo) setClicked(false);
-    if(newTodo) {
+    if (!newTodo) setClicked(false);
+    if (newTodo) {
       await axios.post("http://localhost:3000/todo/updateTodo", {
         todo_id: list.id,
-        new_todo: newTodo
-      })
+        new_todo: newTodo,
+      });
       setClicked(false);
       getTodo();
     }
-  }
+  };
 
   // 삭제 버튼 클릭시
   const clickX = async (list) => {
-    if(confirm("삭제하시겠습니까?")) {
+    if (confirm("삭제하시겠습니까?")) {
       await axios.post("http://localhost:3000/todo/deleteTodo", {
         todo_id: list.id,
-      })
+      });
       getTodo();
-    }else {
+    } else {
       setClicked(false);
     }
-  }
+  };
 
   useEffect(() => {
     getTodo();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    console.log(newTodo)
-  }, [newTodo])
-
+    console.log(newTodo);
+  }, [newTodo]);
 
   // 최종 발표 ver
   return (
@@ -95,15 +93,23 @@ const Index = () => {
         <div>Todo List</div>
       </TodoSubject>
 
-      {todoList &&
+      {todoList && (
         <TodoContainer>
           {/* 미완료 탭 */}
-        {todoList.filter(list => !list.isDone).map((list) => {
-          let color = '#77ADFD';
+          {todoList
+            .filter((list) => !list.isDone)
+            .map((list) => {
+              let color = "#77ADFD";
 
-          return(
+              return (
                 <>
-            <TodoList key={list.id} col={color} onClick={() => {clickContent(list)}}>
+                  <TodoList
+                    key={list.id}
+                    col={color}
+                    onClick={() => {
+                      clickContent(list);
+                    }}
+                  >
                     {clicked ? (
                       <>
                         <TodoContent>
@@ -113,34 +119,63 @@ const Index = () => {
                             placeholder={list.todo}
                           />
                         </TodoContent>
-                <TodoButton onClick={(e) => {e.stopPropagation()}}>
-                  <TodoButton2 col={'#77ADFD'} onClick={() => {clickV(list)}}>V</TodoButton2>
-                  <TodoButton2 col={'red'} onClick={() => {clickX(list)}}>X</TodoButton2>
+                        <TodoButton
+                          onClick={(e) => {
+                            e.stopPropagation();
+                          }}
+                        >
+                          <TodoButton2
+                            col={"#77ADFD"}
+                            onClick={() => {
+                              clickV(list);
+                            }}
+                          >
+                            V
+                          </TodoButton2>
+                          <TodoButton2
+                            col={"red"}
+                            onClick={() => {
+                              clickX(list);
+                            }}
+                          >
+                            X
+                          </TodoButton2>
                         </TodoButton>
                       </>
                     ) : (
                       <>
                         <TodoContent>{list.todo}</TodoContent>
                         <TodoButton>
-                    <img src={btn1} onClick={(e) => {
+                          <img
+                            src={btn1}
+                            onClick={(e) => {
                               e.stopPropagation();
                               handleTodoBtn(list);
-                    }}></img>
+                            }}
+                          ></img>
                         </TodoButton>
                       </>
                     )}
                   </TodoList>
                 </>
-          )
+              );
             })}
 
           {/* 완료 탭 */}
-        {todoList.filter(list => list.isDone).map((list) => {
-          let color = 'lightgray';
+          {todoList
+            .filter((list) => list.isDone)
+            .map((list) => {
+              let color = "lightgray";
 
-          return(
+              return (
                 <>
-            <TodoList key={list.id} col={color} onClick={() => {clickContent(list)}}>
+                  <TodoList
+                    key={list.id}
+                    col={color}
+                    onClick={() => {
+                      clickContent(list);
+                    }}
+                  >
                     {clicked ? (
                       <>
                         <TodoContent>
@@ -150,29 +185,49 @@ const Index = () => {
                             placeholder={list.todo}
                           />
                         </TodoContent>
-                <TodoButton onClick={(e) => {e.stopPropagation()}}>
-                  <TodoButton2 col={'#77ADFD'} onClick={() => {clickV(list)}}>V</TodoButton2>
-                  <TodoButton2 col={'red'} onClick={() => {clickX(list)}}>X</TodoButton2>
+                        <TodoButton
+                          onClick={(e) => {
+                            e.stopPropagation();
+                          }}
+                        >
+                          <TodoButton2
+                            col={"#77ADFD"}
+                            onClick={() => {
+                              clickV(list);
+                            }}
+                          >
+                            V
+                          </TodoButton2>
+                          <TodoButton2
+                            col={"red"}
+                            onClick={() => {
+                              clickX(list);
+                            }}
+                          >
+                            X
+                          </TodoButton2>
                         </TodoButton>
                       </>
                     ) : (
                       <>
                         <TodoContent>{list.todo}</TodoContent>
                         <TodoButton>
-                    <img src={btn2} onClick={(e) => {
+                          <img
+                            src={btn2}
+                            onClick={(e) => {
                               e.stopPropagation();
                               handleTodoBtn(list);
-                    }}></img>
+                            }}
+                          ></img>
                         </TodoButton>
                       </>
                     )}
                   </TodoList>
                 </>
-          )
+              );
             })}
-
         </TodoContainer>
-      }
+      )}
       {isMobile && <BottomBar />}
     </Container>
   );
--- src/pages/TodoList/style.js
+++ src/pages/TodoList/style.js
@@ -5,14 +5,17 @@ export const Container = styled.div`
   height: 100vh;
   padding: 0 4vw;
   box-sizing: border-box;
-  display: flex; flex-direction: column;
+  display: flex;
+  flex-direction: column;
   align-items: center;
 `;
 
 export const TodoSubject = styled.div`
   width: calc(100% - 3.6vh);
   height: 10vh;
-  display: flex; align-items: center; justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   text-shadow: 0px 4px 4px gray;
   font-family: Coiny;
   font-size: 44px;
@@ -20,20 +23,22 @@ export const TodoSubject = styled.div`
   font-weight: 400;
   line-height: normal;
   letter-spacing: 1.28px;
-  color: #77ADFD;
+  color: #77adfd;
   -webkit-text-stroke: 3px white;
 
   div {
     margin: 0 20px 0 20px;
   }
-`
+`;
 export const TodoArraow = styled.span`
   cursor: pointer;
-  color: ${(col) => { return col.col; }};
+  color: ${(col) => {
+    return col.col;
+  }};
   &:hover {
     transform: scale(107%);
   }
-`
+`;
 
 export const TodoContainer = styled.div`
   width: 80vh;
@@ -42,71 +47,91 @@ export const TodoContainer = styled.div`
   filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.2));
   overflow-y: auto;
   box-sizing: border-box;
-`
+
+  @media screen and (max-width: 768px) {
+    width: 80%;
+  }
+`;
 
 export const TodoList = styled.div`
   width: 100%;
   /* height: 6vh; */
   background-color: white;
   /* border: 2px solid #77ADFD; */
-  border: ${(col) => { return `2px solid ${col.col}`; }};
+  border: ${(col) => {
+    return `2px solid ${col.col}`;
+  }};
   border-radius: 12px;
   margin: 0 0 20px 0;
   padding: 20px;
   box-sizing: border-box;
-  display: flex; justify-content: center; align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   &:hover {
-    box-shadow: 0 2px 8px ${(col) => { return col.col}};
+    box-shadow: 0 2px 8px
+      ${(col) => {
+        return col.col;
+      }};
     cursor: pointer;
   }
-`
+`;
 
 export const TodoContent = styled.div`
   width: calc(100% - 8vh);
   height: 100%;
   font-size: 20px;
-  display: flex; align-items: center;
+  display: flex;
+  align-items: center;
   padding: 0 20px 0 10px;
   overflow: hidden;
 
   input {
-    width: 100%; height: 100%;
-    margin: 0; padding: 0;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
     border: none;
     outline: none;
     font-size: 20px;
   }
-`
+`;
 
 export const TodoButton = styled.div`
   width: 8vh;
   height: 100%;
-  display: flex; justify-content: center; align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   img {
-   width: 40px;
-   height: 40px; 
-
-   &:hover {
-    transform: scale(107%);
-   }
-  }
-`
-export const TodoButton2 = styled.div`
     width: 40px;
     height: 40px;
-    display: flex; justify-content: center; align-items: center;
-    text-shadow: 0px 4px 4px gray;
-    font-size: 34px;
-    font-style: normal;
-    font-weight: bold;
-    line-height: normal;
-    color: ${(col) => { return col.col}};
-    -webkit-text-stroke: 2px white;
-    font-family: Coiny;
 
     &:hover {
       transform: scale(107%);
     }
-`
+  }
+`;
+export const TodoButton2 = styled.div`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-shadow: 0px 4px 4px gray;
+  font-size: 34px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: normal;
+  color: ${(col) => {
+    return col.col;
+  }};
+  -webkit-text-stroke: 2px white;
+  font-family: Coiny;
+
+  &:hover {
+    transform: scale(107%);
+  }
+`;
